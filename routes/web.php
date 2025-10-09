b2730497121f0d65b05aa4caed65fe39be25044e
Leke Abiodun <lekepeterabiodun@gmail.com>
2025-10-09 03:10:30 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Livewire\Dashboard;
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+use Livewire\Volt\Volt;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
+
+Route::redirect('/', 'dashboard');
+Route::group([
+    'middleware' => ['auth', 'verified'],
+    // 'prefix' => 'dashboard',
+    // 'as' => 'dashboard.',
+], function () {
+    Route::get('/', Dashboard::class)->name('dashboard.index');
+    Route::get('/projects', App\Livewire\Project\Index::class)->name('projects.index');
+    Route::get('/projects/{id}', App\Livewire\Project\Show::class)->name('projects.show');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
+    Volt::route('settings/password', 'settings.password')->name('password.edit');
+    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
+
+    Volt::route('settings/two-factor', 'settings.two-factor')
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('two-factor.show');
+});
+
+require __DIR__ . '/auth.php';

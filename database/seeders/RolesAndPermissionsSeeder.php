b858c3fedf196cd79d47cf77ea8abb46b3998a4a
Leke Abiodun <lekepeterabiodun@gmail.com>
2025-11-14 12:22:27 +0100

    Update RolesAndPermissionsSeeder.php

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -219,6 +219,15 @@ class RolesAndPermissionsSeeder extends Seeder
             'view documents',
             'add comments',
             'view comments',
+            'clock in',
+            'clock out',
+            'view time entries',
+            'manage time entries',
+            'view leave requests',
+            'create leave requests',
+            'edit leave requests',
+            'approve leave requests',
+            'reject leave requests',
         ]);
 
         // 9. Client - View only, no materials
